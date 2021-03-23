0bd97d37def6ebfac4dd5661b93bf6c35db56776
i-salama <ibrah.salama@outlook.com>
2021-03-23 04:52:00 +0200

    building nav-par

--- js/app.js
+++ js/app.js
@@ -17,15 +17,21 @@
  * Define Global Variables
  * 
 */
-
-
+//select all setions in dom
+var sections = document.querySelectorAll('.section')
+//select the nav bar 
+var navigationList  = document.querySelector('#myList')
+//create a new empty document docFragment
+var docFragment = document.createDocumentFragment();
+//select all links 
+const alinks = document.getElementsByTagName('a')
+//select all li
+const li = document.getElementsByTagName('li')
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-
-
 
 /**
  * End Helper Functions
@@ -36,22 +42,72 @@
 // build the nav
 
 
-// Add class 'active' to section when near top of viewport
+        //for each section create li,
+        //for each li append link,
+        //for each 
+sections.forEach((element)=>{
+    //first we need to get the data nave attribute
+    //to add it as a text node for each section
+    const secAttribute = element.getAttribute('data-nav');
+    //create li
+    const li = document.createElement('li');
+    //create link
+    const link = document.createElement('a');
+    //create text node
+    const sectionText = document.createTextNode(secAttribute);
+    //alert(secAttribute);
+    //Add event on each link created to scroll to the desired section 
+    link.addEventListener('click',(event)=>{
+        event.preventDefault();
+        element.scrollIntoView({behavior:'smooth',block: 'center'})
+    })
+    //append the text of section attribute to link
+    link.appendChild(sectionText);
+    //append each link to the li 
+    li.appendChild(link);
+    //append li to the fragment 
+    docFragment.appendChild(li);
+    // console.log(docFragment);
+})
+                //append the fragment to ul navigation list 
+navigationList.appendChild(docFragment)
 
+//window action
+window.addEventListener('scroll', toggleActiveState);
 
-// Scroll to anchor ID using scrollTO event
+//intersection object 
+const option={
+    root: null,
+    threshold:0.5,
+    rootMargin: "0px 0px -200px 0px"
+};
+function toggleActiveState(){
+  //  instance of intersectionObserver
+    const interActObserv = new IntersectionObserver((entries,observer)=>{
+        
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return
+            }
+            //console.log(entry.target.dataset.nav);
+            // add new class to one of the entries and disable the others 
+            sections.forEach((section)=>{   
+                section.classList.remove('your-active-class')
+                if( entry.target.getAttribute('data-nav')== section.getAttribute('data-nav')){
+                    section.classList.add('your-active-class')
+                }
+            })
 
+            for(let i= 0 ; i< 4 ; i++){
+                // console.log(alinks[i])
+                alinks[i].classList.remove('item-active-class')
+                // console.log(alinks[i].textContent)
+                if(alinks[i].textContent == entry.target.getAttribute('data-nav')){
+                    alinks[i].classList.add('item-active-class')
+                }
+            }
 
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+        })
+    },option)
+    sections.forEach((sec)=>{ interActObserv.observe(sec)})
+}
